f2e60e468e4e36802b56364f8cba098c0c4843ca
code <rythewuff@gmail.com>
2018-12-11 04:19:12 -0600

    Embed Links to User

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ client.on('ready', () => {
     console.log('Both Bots ready!')
 })
 let e = new D.RichEmbed()
+let r = new D.RichEmbed()
 client.on('message', message => {
     if (message.author.bot) return;
     if (message.author.id !== '318044130796109825') return;
@@ -40,13 +41,12 @@ client.on('message', message => {
     let token = config.tokenJin.replace(/\./g, "\.")
     let tooken = new RegExp(token, 'g')
     ev = ev.replace(tooken, `haha yes`);
-    let e = new D.RichEmbed()
-        e.addField("Input", "```js\n" + code + "```")
+        r.addField("Input", "```js\n" + code + "```")
         .addField("Eval", "```js\n"+ev+"```")
         .setColor(0xf24946)
-        message.channel.send(e)
+        message.channel.send(r)
   } catch(err) {
-    message.channel.send(e.setDescription("```js\n" + err + "```"));
+    message.channel.send(r.setDescription("```js\n" + err + "```"));
   }
 }
 
@@ -61,20 +61,41 @@ client.on('message', message => {
 //       })
 //   }
 })
-var users = ["2839919909"];
-var stream = T.stream('statuses/filter', {track: '#codepupperBot, #commissions, #commissionsopen, #furry, #furryart, commissions open'});
+
+var users = ["2839919909"]; // Das me#
+
+let Blacklist = ["163818811"]
+var stream = T.stream('statuses/filter', {track: '#codepupperBot, #commissions, #commissionsopen'});
 stream.on('tweet', function (tweet) {
-    if (users.indexOf(tweet.user.id_str) > -1) {
+    if(tweet.user.name === 'Kaito') return;
+    if(tweet.text.startsWith('RT')) return;
+    if(!tweet.text.includes('furry')) return console.log('Does not include furry, skipping...');
         console.log(tweet.user.name + ": " + tweet.text);
-        e.setAuthor(tweet.user.name, tweet.user.profile_image_url_https)
+        e.setAuthor(tweet.user.name, tweet.user.profile_image_url_https, 'https://twitter.com/' + tweet.user.screen_name)
          .setDescription(tweet.text)
          .setColor(tweet.user.profile_link_color)
          console.log(tweet.entities.user_mentions || 'None')
         client.channels.get('520213445119836171').send(e);
         T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
-            // console.log(data)
+      console.log(data)
         })
     }
-})
+)
+
+// var art = T.stream('statuses/filter', {track: '#furry, #furryart'});
+// art.on('tweet', function (tweet) {
+//     if(tweet.user.name === 'Kaito') return;
+//     if(tweet.text.startsWith('RT')) return;
+//         console.log(tweet.user.name + ": " + tweet.text);
+//         e.setAuthor(tweet.user.name, tweet.user.profile_image_url_https)
+//          .setDescription(tweet.text)
+//          .setColor(tweet.user.profile_link_color)
+//          console.log(tweet.entities.user_mentions || 'None')
+//         client.channels.get('521005893366841385').send(e);
+//         T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
+//             // console.log(data)
+//         })
+//     }
+// )
 
 client.login(config.tokenJin)
